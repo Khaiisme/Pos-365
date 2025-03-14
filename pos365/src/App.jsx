ae5e191b8fd4199e8c647bd810533a86c5f47b93
Van Khai <kn579309@gmail.com>
2025-03-14 16:05:30 +0100

    First time

---
+++ pos365/src/App.jsx
@@ -0,0 +1,352 @@
+import { useState } from "react";
+import Table from "./components/Table";
+import Modal from "./components/Modal";
+
+// Sample dish data (name and price)
+const dishes = [
+  { name: "01.GemuseSuppe", price: 4.5 },
+  { name: "02.HühnerSuppe", price:  5.5},
+  { name: "03.GarnellenSuppe", price: 6.5 },
+  { name: "04.FishSuppe", price:6.5  },
+  { name: "05.VantanSuppe", price:6  },
+  { name: "06a.GlasnudelsupTofu", price: 5 },
+  { name: "06b.GlasnudelsupHühn", price: 6 },
+  { name: "06c.GlasnudelsupGarnel", price:6.5  },
+  { name: "07a.TomSuppe Tofu", price: 5 },
+  { name: "07a.TomSuppe Hühner", price: 6 },
+  { name: "08.TomJun", price: 7.5  },
+  { name: "11a", price: 4.5 },
+  { name: "11b", price: 5.5 },
+  { name: "12a", price: 4.5 },
+  { name: "12b", price: 5.5 },
+  { name: "13", price: 7.5 },
+  { name: "14.Vantan gebacken", price: 6.5 },
+  { name: "15", price: 6.5 },
+  { name: "16.GemüseSalat ", price: 4.5 },
+  { name: "17a.MangoSalat", price: 5.5 },
+  { name: "17b.Mangosalat", price: 7.5 },
+  { name: "18.Hühnersalat", price: 6 },
+  { name: "19.Rindersalat", price: 6.5 },
+  { name: "20a", price: 6 },
+  { name: "20b", price: 7 },
+  { name: "20c", price: 7.5 },
+  { name: "09a.GlasnudelnSupp Tofu", price: 9 },
+  { name: "09b.GlasnudelnSupp Hühner", price: 10 },
+  { name: "09e.GlasnudelnSupp Garnelen", price: 14},
+  { name: "09f.GlasnudelnSupp ", price:  18},
+  { name: "10a.Gebratenglasnudel Tofu", price: 9 },
+  { name: "10b.Gebratenglasnudel Hühner", price: 10 },
+  { name: "10c.Gebratenglasnudel Rind", price: 13 },
+  { name: "10d.Gebratenglasnudel Enten", price: 11 },
+  { name: "10e.Gebratenglasnudel Garnelen", price: 14 },
+  { name: "10f.Gebratenglasnudel ", price: 18 },
+  { name: "21a.Pho Tofu", price: 9 },
+  { name: "21b.Pho Hühner", price: 10 },
+  { name: "21c.Pho Rind", price: 13 },
+  { name: "21f.Pho Special", price: 13 },
+  { name: "29.Bo Sot Vang", price: 14 },
+  { name: "52a.Curry Reisbandnudel Tofu", price:10  },
+  { name: "52b.Curry Reisbandnudel Hühner", price:11  },
+  { name: "52c.Curry Reisbandnudel Rind", price: 13 },
+  { name: "52e.Curry Reisbandnudel Garnel", price:14  },
+  { name: "52f.Curry Reisbandnudel Special", price: 18 },
+  { name: "22a.Gebraten Nudeln Tofu", price: 10 },
+  { name: "22b.Gebraten Nudeln Hühner", price: 11 },
+  { name: "22c.Gebraten Nudeln Rind", price: 13 },
+  { name: "22d.Gebraten Nudeln Enten", price: 12 },
+  { name: "22e.Gebraten Nudeln Garnel", price:  14},
+  { name: "22f.Gebraten Nudeln Special", price: 18 },
+  { name: "23a.Pho Hanoi Tofu", price: 9 },
+  { name: "23b.Pho Hanoi Hühner", price: 11 },
+  { name: "23c.Pho Hanoi Fisch", price: 14 },
+  { name: "25.", price: 13 },
+  { name: "26.Bun Tofu gebacken", price: 10 },
+  { name: "27.Bun cha Hanoi", price: 12 },
+  { name: "28.Bun nem Hanoi", price: 11 },
+  { name: "30", price: 22 },
+  { name: "31", price: 14 },
+  { name: "32a.Gebraten Udom Tofu ", price: 10 },
+  { name: "32b.Gebraten Udom Hühner", price: 11 },
+  { name: "32c.Gebraten Udom Rind", price: 13 },
+  { name: "32d.Gebraten Udom Enten", price: 11 },
+  { name: "32e.Gebraten Udom Garnel", price: 14 },
+  { name: "32f.Gebraten Udom Special", price: 18 },
+  { name: "33a.RotCurry Tofu", price: 9 },
+  { name: "33b.RotCurry Hühner", price: 10 },
+  { name: "33c.RotCurry Rind", price: 13 },
+  { name: "33d.RotCurry Enten", price: 11 },
+  { name: "33e.RotCurry Garnel", price: 14 },
+  { name: "34a.MangoSauce Tofu", price: 9 },
+  { name: "34b.MangoSauce Hühner", price: 10 },
+  { name: "34c.MangoSauce Rind", price: 11 },
+  { name: "35a.ErdnussSauce Tofu", price: 9 },
+  { name: "35b.ErdnussSauce Hühner", price:10  },
+  { name: "35c.ErdnussSauce Rind", price: 13 },
+  { name: "35d.ErdnussSauce Enten", price: 11},
+  { name: "35e.ErdnussSauce Garnel", price: 14 },
+  { name: "36a.Pho Sauce Tofu", price: 9 },
+  { name: "36b.Pho Sauce Hühner", price:10  },
+  { name: "36c.Pho Sauce Rind", price: 13 },
+  { name: "36d.Pho Sauce Enten", price:11  },
+  { name: "36e.Pho Sauce Garnel", price: 14 },
+  { name: "37a.XaoXaOt Tofu", price: 9 },
+  { name: "37b.XaoXaOt Hühner", price: 10 },
+  { name: "37c.XaoXaOt Rind", price: 13 },
+  { name: "37d.XaoXaOt Enten", price: 11 },
+  { name: "37e.XaoXaOt Garnel", price: 14 },
+  { name: "38.", price: 13 },
+  { name: "39.Gegrillte Garnelen", price: 40 },
+  { name: "40.", price: 40 },
+  { name: "41a.Knusprige Hühner", price: 11 },
+  { name: "41b.Knusprige Hühner", price: 11 },
+  { name: "41c.Knusprige Hühner", price: 11 },
+  { name: "41d.Knusprige Hühner", price: 11 },
+  { name: "41e.Knusprige Hühner", price: 11 },
+  { name: "42a.Knusprige Ente", price: 14 },
+  { name: "42b.Knusprige Ente", price: 14 },
+  { name: "42c.Knusprige Ente", price: 14 },
+  { name: "42d.Knusprige Ente", price: 14 },
+  { name: "42e.Knusprige Ente", price: 14 },
+  { name: "43a.Knusprige Hühnerschenkel", price: 12 },
+  { name: "43b.Knusprige Hühnerschenkel", price: 12 },
+  { name: "43c.Knusprige Hühnerschenkel", price: 12 },
+  { name: "43d.Knusprige Hühnerschenkel", price: 12 },
+  { name: "43e.Knusprige Hühnerschenkel", price: 12 },
+  { name: "46a.Fisch gebacken", price: 14 },
+  { name: "46b.Fisch gebacken", price: 14 },
+  { name: "46c.Fisch gebacken", price: 14 },
+  { name: "46d.Fisch gebacken", price: 14 },
+  { name: "46e.Fisch gebacken", price: 14 },
+  { name: "47a.GebratenNudeln Tofu", price: 9 },
+  { name: "47b.GebratenNudeln Hühner", price: 10 },
+  { name: "47c.GebratenNudeln Rind", price: 13 },
+  { name: "47d.GebratenNudeln Enten", price: 11 },
+  { name: "47e.GebratenNudeln Garnel", price: 14 },
+  { name: "47f.GebratenNudeln knus Hühner", price: 12 },
+  { name: "47g.GebratenNudeln knus Enten", price: 14 },
+  { name: "48a.GebratenReis Tofu", price: 9 },
+  { name: "48b.GebratenReis Hühner", price: 10 },
+  { name: "48c.GebratenReis Rind", price: 13 },
+  { name: "48d.GebratenReis Enten", price: 11 },
+  { name: "48e.GebratenReis Garnel", price: 14 },
+  { name: "48f.GebratenReis knus Hühner", price: 12 },
+  { name: "48g.GebratenReis knus Ente", price: 14 },
+  { name: "U1 mit Tofu", price:10  },
+  { name: "U2 mit Hühner", price: 11 },
+  { name: "U3 mit Rind", price: 13 },
+  { name: "U4 mit Enten", price: 12 },
+  { name: "U5 mit Garnel", price: 15 },
+  { name: "U6 mit Fisch", price: 15 },
+  { name: "U7 mit knusprige Hühner", price: 14 },
+  { name: "U8 mit knusprige Enten", price: 15 },
+  { name: "R1 mit Tofu", price:10  },
+  { name: "R2 mit Hühner", price: 11 },
+  { name: "R3 mit Rind", price: 13 },
+  { name: "R4 mit Enten", price: 12 },
+  { name: "R5 mit Garnel", price: 15 },
+  { name: "R6 mit Fisch", price: 15 },
+  { name: "R7 mit knusprige Hühner", price: 14 },
+  { name: "R8 mit knusprige Enten", price: 15 },
+  { name: "G1 mit Tofu", price:10  },
+  { name: "G2 mit Hühner", price: 11 },
+  { name: "G3 mit Rind", price: 13 },
+  { name: "G4 mit Enten", price: 12 },
+  { name: "G5 mit Garnel", price: 15 },
+  { name: "G6 mit Fisch", price: 15 },
+  { name: "G7 mit knusprige Hühner", price: 14 },
+  { name: "G8 mit knusprige Enten", price: 15 },
+  { name: "B1 mit Tofu", price:10  },
+  { name: "B2 mit Hühner", price: 11 },
+  { name: "B3 mit Rind", price: 13 },
+  { name: "B4 mit Enten", price: 12 },
+  { name: "B5 mit Garnel", price: 15 },
+  { name: "B6 mit Fisch", price: 15 },
+  { name: "B7 mit knusprige Hühner", price: 14 },
+  { name: "B8 mit knusprige Enten", price: 15 },
+  { name: "USC 1 mit Tofu", price: 10 },
+  { name: "USC 2 mit Hühner", price: 12 },
+  { name: "USC 3 mit Rind", price: 14 },
+  { name: "USC 4 mit Garnel", price: 16 },
+  { name: "US 1 mit Tofu", price: 10 },
+  { name: "US 2 mit Hühner", price: 12 },
+  { name: "US 3 mit Rind", price: 14 },
+  { name: "US 4 mit Garnelen", price: 16 },
+  { name: "V1", price: 14 },
+  { name: "V2", price: 14 },
+  { name: "V3", price: 14 },
+  { name: "V4", price: 14 },
+  { name: "V5", price: 14 },
+  { name: "D1", price: 12 },
+  { name: "D2", price: 12 },
+  { name: "D3", price: 12 },
+  { name: "D4", price: 12 },
+  { name: "D5", price: 12 },
+  { name: "49.Gebacken Banana", price: 3.9 },
+  
+  { name: "Grüner Tee", price: 2.8 },
+  { name: "Jasmin Tee", price: 2.8 },
+  { name: "Ingwer Tee", price: 3.5 },
+  { name: "Kumquat Tee", price: 3.5 },
+  { name: "Limet-Zitron Tee", price: 3.5 },
+  { name: "Kaffe ", price: 2.5 },
+  { name: "Capuchino", price:2.5  },
+  { name: "Latte Machiato", price: 3 },
+  { name: "Espresso", price: 2.2 },
+  { name: "Kaffe VietNam", price: 4.5 },
+  { name: "Wasser Still", price: 2.5 },
+  { name: "Mineralwasser", price: 2.5 },
+  { name: "Coca Cola ", price: 2.5 },
+  { name: "Coca Light ", price: 2.5 },
+  { name: "Coca Zero ", price: 2.5 },
+  { name: "Spezi ", price: 2.5 },
+  { name: "Fanta ", price: 2.5 },
+  { name: "Sprite ", price: 2.5 },
+  { name: "Wasser Still", price:3.8 },
+  { name: "Mineralwasser", price: 3.8 },
+  { name: "Coca Cola ", price: 3.8 },
+  { name: "Coca Light ", price: 3.8 },
+  { name: "Coca Zero ", price: 3.8 },
+  { name: "Spezi ", price: 3.8 },
+  { name: "Fanta ", price: 3.8 },
+  { name: "Sprite ", price:3.8 },
+  { name: "Ginger Ale", price: 3 },
+  { name: "Bitte Lemon", price: 3 },
+  { name: "Tonic Wasser", price: 3 },
+  { name: "Apfelschorle", price: 3 },
+  { name: "Ginger Ale", price: 4 },
+  { name: "Bitte Lemon", price: 4 },
+  { name: "Tonic Wasser", price: 4 },
+  { name: "Apfelschorle", price: 4 },
+  { name: "Orangesaft", price: 3 },
+  { name: "Apfelsaft", price: 3 },
+  { name: "Annasaft", price: 3 },
+  { name: "Bananasaft", price: 3 },
+  { name: "Kirschsaft", price: 3 },
+  { name: "Mangosaft", price: 3 },
+  { name: "Maracujasaft", price: 3 },
+  { name: "Guavensaft", price: 3 },
+  { name: "Litschisaft", price: 3 },
+  { name: "Kiba", price: 3 },
+  { name: "Orangesaft", price: 4 },
+  { name: "Apfelsaft", price: 4 },
+  { name: "Annasaft", price: 4 },
+  { name: "Bananasaft", price: 4 },
+  { name: "Kirschsaft", price: 4 },
+  { name: "Mangosaft", price: 4 },
+  { name: "Maracujasaft", price: 4 },
+  { name: "Guavensaft", price: 4},
+  { name: "Litschisaft", price: 4 },
+  { name: "Kiba", price: 4 },
+
+  { name: "Jever Klein", price: 3 },
+  { name: "Radler Klein ", price: 3 },
+  { name: "Diesel Klein", price: 3 },
+  { name: "Jever Groß", price: 4.2 },
+  { name: "Radler Groß", price: 4.2 },
+  { name: "Diesel Groß", price: 4.2 },
+  { name: "Taiger Bier ", price: 3.8 },
+  { name: "Saigon Bier", price: 3.8 },
+  { name: "Hefeweizen ", price: 3.8 },
+  { name: "Hefeweizen Dunkel", price: 3.8 },
+  { name: "Hefeweizen Kristall", price: 3.8 },
+  { name: "Hefeweizen Alkoholfrei", price: 3.8 },
+  { name: "1 Shot Alkohol ", price: 2.6 },
+  { name: "Chardonay ", price: 6 },
+  { name: "Riesling ", price: 6 },
+  { name: "Sauvignon ", price: 6 },
+  { name: "Grauburgrunder ", price: 5.5 },
+  { name: "Müller Thurgau ", price: 6 },
+  { name: "Flasche von Weißwein/Rotwein", price: 19.9 },
+  { name: "Merlot", price: 6 },
+  { name: "Bordeaux", price: 6 },
+
+];
+
+const App = () => {
+  // Read from localStorage and set the initial state for tables and orders
+  const storedTables = JSON.parse(localStorage.getItem("tables")) || Array.from({ length: 11 }, (_, i) => i + 1);
+  const storedOrders = JSON.parse(localStorage.getItem("orders")) || {};
+
+  const [tables, setTables] = useState(storedTables);
+  const [orderItems, setOrderItems] = useState(storedOrders);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentTable, setCurrentTable] = useState(null);
+  const [abholungCount, setAbholungCount] = useState(0);
+
+  // Add an "Abholung" table with a dynamic name (e.g., Abholung 1, Abholung 2)
+  const addTable = () => {
+    const newAbholungName = `Abholung ${abholungCount + 1}`;
+    const updatedTables = [...tables, newAbholungName];
+    setTables(updatedTables);
+    localStorage.setItem("tables", JSON.stringify(updatedTables)); // Save to localStorage
+
+    setAbholungCount(abholungCount + 1);
+  };
+
+  // Handle clicking on a table to open the modal and reset order items
+  const handleTableClick = (tableName) => {
+    setCurrentTable(tableName);
+    setIsModalOpen(true);
+    setOrderItems(storedOrders[tableName] || []); // Load the saved order items for the selected table
+  };
+
+  // Add order item (dish) to the list
+  const addOrderItem = (name, price) => {
+    const newOrderItem = { name, price };
+    const updatedOrderItems = [...orderItems, newOrderItem];
+    setOrderItems(updatedOrderItems);
+
+    // Save the updated order items to localStorage for the specific table
+    const updatedOrders = { ...storedOrders, [currentTable]: updatedOrderItems };
+    localStorage.setItem("orders", JSON.stringify(updatedOrders));
+  };
+
+  // Remove order item from the list
+  const removeOrderItem = (index) => {
+    const updatedOrderItems = orderItems.filter((_, i) => i !== index);
+    setOrderItems(updatedOrderItems);
+
+    // Update the order in localStorage for the specific table
+    const updatedOrders = { ...storedOrders, [currentTable]: updatedOrderItems };
+    localStorage.setItem("orders", JSON.stringify(updatedOrders));
+  };
+
+  return (
+    <div className="w-full max-w-[412px] h-[915px] mx-auto bg-white text-black flex flex-col items-center p-10">
+      <h1 className="text-3xl font-bold mb-10">Hanoi Pho</h1>
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 w-full">
+        {tables.map((table, index) => (
+          <div
+            key={index}
+            className="cursor-pointer"
+            onClick={() => handleTableClick(table)}
+          >
+            <Table number={table} />
+          </div>
+        ))}
+      </div>
+      <button
+        onClick={addTable}
+        className="mt-6 bg-blue-500 text-black py-2 px-4 rounded"
+      >
+        Abholung
+      </button>
+
+      {/* Modal */}
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        tableName={currentTable}
+        orderItems={orderItems}
+        setOrderItems={setOrderItems}
+        tables={tables}
+        setTables={setTables}
+        addOrderItem={addOrderItem}
+        removeOrderItem={removeOrderItem}
+        dishes={dishes}
+      />
+    </div>
+  );
+};  
+
+export default App;
---
+++ pos365/src/components/Modal.jsx
@@ -0,0 +1,222 @@
+import { useState, useRef, useEffect } from "react";
+
+const Modal = ({
+  isOpen,
+  onClose,
+  tableName,
+  orderItems,
+  setOrderItems,
+  setTables,
+  addOrderItem,
+  removeOrderItem,
+  dishes,
+}) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [showDishes, setShowDishes] = useState(false); // To control the visibility of the filtered dishes
+  const modalRef = useRef(null); // Reference for the modal
+  const searchWrapperRef = useRef(null); // Reference for the search wrapper
+  const [note, setNote] = useState(localStorage.getItem(tableName) || "");
+  // Calculate the total
+  const calculateTotal = () => {
+    return orderItems.reduce((total, item) => total + item.price, 0);
+  };
+
+  // Filter dishes based on the search query
+  const filteredDishes = dishes.filter((dish) =>
+    dish.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  // Handle search input changes
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+    setShowDishes(true); // Show dishes when the user starts typing
+  };
+
+  // Close the dropdown if clicking outside the search bar
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (
+        searchWrapperRef.current &&
+        !searchWrapperRef.current.contains(event.target)
+      ) {
+        setShowDishes(false); // Hide the dropdown when clicking outside
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+
+  // Handle double-click on an order item
+  const [clickTimer, setClickTimer] = useState(null); // Timer to handle double click
+  const handleItemClick = (index) => {
+    if (clickTimer) {
+      clearTimeout(clickTimer); // Clear the previous timeout
+      setClickTimer(null); // Reset the timer
+      // Remove item if double clicked
+      removeOrderItem(index);
+    } else {
+      const newTimer = setTimeout(() => {
+        setClickTimer(null); // Reset the timer after the timeout
+      }, 300); // 300ms to detect double click
+      setClickTimer(newTimer);
+    }
+  };
+
+  // Handle the "Pay" button click
+  const handlePay = () => {
+    if (!tableName) {
+      console.error("Table name is undefined!");
+      return;
+    }
+
+    // Retrieve current orders from localStorage
+    const storedOrders = JSON.parse(localStorage.getItem("orders")) || {};
+    const storedNotes = JSON.parse(localStorage.getItem("notes")) || {};
+    // Remove the specific table's order
+    delete storedOrders[tableName];
+    delete storedNotes[tableName];
+    // Save updated orders back to localStorage
+    localStorage.setItem("orders", JSON.stringify(storedOrders));
+    localStorage.setItem("notes", JSON.stringify(storedNotes));
+    // Clear order items in state
+    setOrderItems([]);
+    setNote("");
+
+    // Remove "Abholung" table if it exists
+    if (tableName.includes("Abholung")) {
+      setTables((prevTables) =>
+        prevTables.filter((table) => table !== tableName)
+      );
+    }
+
+    // Close the modal
+    onClose();
+  };
+
+  // Retrieve the stored note for this specific table from localStorage or use an empty string if no note is found
+  
+
+  useEffect(() => {
+    if (tableName) {
+      const storedNotes = JSON.parse(localStorage.getItem("notes")) || {};
+      setNote(storedNotes[tableName] || ""); // Load the note for the selected table
+    }
+  }, [tableName]); // Runs when a new table is opened
+
+  useEffect(() => {
+    if (tableName) {
+      const storedNotes = JSON.parse(localStorage.getItem("notes")) || {}; // Get all notes
+      if (note.trim() === "") {
+        delete storedNotes[tableName]; // Remove note if empty
+      } else {
+        storedNotes[tableName] = note; // Save note for the table
+      }
+      localStorage.setItem("notes", JSON.stringify(storedNotes));
+    }
+  }, [note, tableName]);
+
+  return (
+    isOpen && (
+      <div className="text-xl fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+        <div
+          ref={modalRef}
+          className="bg-white p-6 rounded-lg w-full h-full sm:max-w-md sm:h-auto relative"
+        >
+          {/* Close button (X) */}
+          <button
+            onClick={() => {
+              onClose();
+            }}
+            className="absolute mt-12 top-2 right-2 text-2xl font-bold text-black hover:text-gray-700"
+          >
+            X
+          </button>
+
+          <h2 className="text-3xl font-bold mb-4 mt-10">Tisch {tableName}</h2>
+
+          {/* Search Bar */}
+          <div className="mb-4 relative" ref={searchWrapperRef}>
+            <input
+              type="text"
+              placeholder="Search for a dish..."
+              value={searchQuery}
+              onChange={handleSearchChange}
+              className="w-full p-2 border border-gray-300 rounded"
+            />
+            {/* Display filtered dishes only if showDishes is true */}
+            {showDishes && searchQuery && filteredDishes.length > 0 && (
+              <div className="absolute left-0 right-0 max-h-48 overflow-y-auto bg-white border border-gray-300 rounded mt-1 z-10">
+                {filteredDishes.map((dish, index) => (
+                  <div
+                    key={index}
+                    className="flex justify-between items-center p-2 cursor-pointer hover:bg-gray-200"
+                    onClick={() => {
+                      addOrderItem(dish.name, dish.price); // Add the item to the order
+                      setShowDishes(false); // Close the dropdown
+                      setSearchQuery(""); // Clear the search query
+                    }}
+                  >
+                    <span>{dish.name}</span>
+                    <span>{dish.price}€</span>
+                  </div>
+                ))}
+              </div>
+            )}
+
+            {/* If no dishes match the search query, display a "No results" message */}
+            {showDishes && searchQuery && filteredDishes.length === 0 && (
+              <div className="absolute left-0 right-0 p-2 text-center text-gray-500">
+                No results found
+              </div>
+            )}
+          </div>
+
+          {/* Note Box */}
+          <div>
+            <textarea
+              value={note}
+              onChange={(e) => setNote(e.target.value)} // Update the note as the user types
+              rows="1" // Only 1 line
+              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 resize-none"
+              placeholder="Write your note here..."
+            ></textarea>
+          </div>
+
+          {/* Order Items */}
+          <div className="mt-2 p-2 border border-gray-300 rounded-lg shadow-lg bg-white">
+            {orderItems.map((item, index) => (
+              <div
+                key={index}
+                className="flex justify-between items-center p-2 cursor-pointer border-b last:border-0 hover:bg-gray-100"
+                onClick={() => handleItemClick(index)} // Handle double-click to remove item
+              >
+                <span className="font-semibold">{item.name}</span>
+                <span className="font-semibold">{item.price}€</span>
+              </div>
+            ))}
+          </div>
+
+          {/* Total */}
+          <div className="mt-4 font-bold bg-blue-300 w-full rounded-lg p-4">
+            Ingesamt:<span className="text-2xl ml-50">{calculateTotal()}€</span>
+          </div>
+
+          {/* Pay Button */}
+          <div className="bg-gray-600 mt-6">
+            <button
+              onClick={handlePay}
+              className=" text-black text-4xl py-2 px-4 rounded-xl w-full"
+            >
+              Bezahlen
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
+
+export default Modal;
